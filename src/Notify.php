4d34a1a20fe33fa598a088e071e89460b5f6dd40
Me Proficiency <58886077+mexproficiency@users.noreply.github.com>
2019-12-15 00:11:56 +0700

    Initial commit

---
+++ src/Notify.php
@@ -0,0 +1,60 @@
+<?php
+namespace W2W\LINE;
+
+use GuzzleHttp\Client;
+
+class Notify {
+	const conAPIURLADR = "https://notify-api.line.me/api/notify";
+	
+	private $strLINETOKEN = null;
+	private $strLINEHTTPS = null;
+	
+	public function __construct($strLINETOKEN) {
+		$this->token = $strLINETOKEN;
+		$this->http = new Client();
+	}
+	
+	public function setToken($strLINETOKEN) {
+		$this->token = $strLINETOKEN;
+	}
+	
+	public function getToken() {
+		return $this->token;
+	}
+	
+	public function send($strINPTXTMSG, $strINPTXTIMG = null, $strINPTXTSTK = null) {
+		if(empty($strINPTXTMSG)) {
+			return false;
+		}
+		
+		$strREQUESTXX = ["headers" => ["Authorization" => "Bearer " . $this->token]];
+		$strREQUESTXX["multipart"] = [["name" => "message", "contents" => $strINPTXTMSG]];
+		
+		if(!empty($strINPTXTIMG) && preg_match("#^https?://#", $strINPTXTIMG)) {
+			$strREQUESTXX["multipart"][] = ["name" => "imageThumbnail", "contents" => $strINPTXTIMG];
+			$strREQUESTXX["multipart"][] = ["name" => "imageFullsize", "contents" => $strINPTXTIMG];
+		} elseif(!empty($strINPTXTIMG) && file_exists($strINPTXTIMG)) {
+			$strREQUESTXX["multipart"][] = ["name" => "imageFile", "contents" => fopen($strINPTXTIMG, "r")];
+		}
+		
+		if(!empty($strINPTXTSTK) && !empty($strINPTXTSTK["stickerPackageId"]) && !empty($strINPTXTSTK["stickerId"])) {
+			$strREQUESTXX["multipart"][] = ["name" => "stickerPackageId", "contents" => $strINPTXTSTK["stickerPackageId"]];
+			$strREQUESTXX["multipart"][] = ["name" => "stickerId", "contents" => $strINPTXTSTK["stickerId"]];
+		}
+		
+		$strRESPONSEX = $this->http->request("POST", Notify::conAPIURLADR, $strREQUESTXX);
+		
+		if($strRESPONSEX->getStatusCode() != 200) {
+			return false;
+		}
+		
+		$strGETTXTBOD = (string)$strRESPONSEX->getBody();
+		$objJSO = json_decode($strGETTXTBOD, true);
+		
+		if(empty($objJSO["status"]) || empty($objJSO["message"])) {
+			return false;
+		}
+		
+		return true;
+	}
+}
